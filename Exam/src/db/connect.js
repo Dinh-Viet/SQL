823d0c9a0c7749affe5ad3d0fddc54246d17fb55
Đinh Việt <115639396+Dinh-Viet@users.noreply.github.com>
2023-11-06 18:43:32 +0700

    Delete Exam/src directory

--- Exam/src/db/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const db_name = 'exam';
-
-mongoose.connect('mongodb://localhost:27017', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = mongoose;
